4362f13b5209dab43048ab1eec4b308f79bfb358
flywithbug <369495368@qq.com>
2019-06-15 14:32:06 +0800

    a

--- request_test.go
+++ request_test.go
@@ -1,9 +1,23 @@
 package http_request
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestApiRequest(t *testing.T) {
-
+	req := NewRequest()
+	url := ""
+	req.SetCookies(map[string]string{})
+	res, err := req.Get(url, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	body, err := res.Body()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(string(body))
 }
